a9da086af96b7150cfcfaf8bb0dcb0b96581d2be
douglasdev <dgsgithub@gmail.com>
2024-05-08 21:59:51 -0300

    atualizacao

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,11 @@
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         
+        String nomeCliente = args [0];
+        String agencia = args [1];
+        int numeroAgencia = Integer.valueOf(args[2]);
+        double saldo = Double.valueOf(args[3]);
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numeroAgencia + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
